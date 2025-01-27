1bcb8884ed99a7c9b7bc12f3bae209650deb83fd
Peter Freedman <frdmanp@gmail.com>
2025-01-26 22:41:57 -0500

    database progress - began work on add character and item methods for db - restructured db so that inventory tables have a unique primary key - restructured db so that the item table now has nullable optional traits instead of dynamic polymorphism.

--- app.py
+++ app.py
@@ -13,24 +13,39 @@ db.init_app(app)
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        char = Character(request.form['name'], request.form['hp'], request.form['weight'])
-        db.session.add(char)
-        db.session.commit()
+        if request.form['hp']:
+            add_character(request.form)
 
-        print(db.session.query(Character).all())
-        
-        #DEBUG CODE PLEASE REMOVE
-        with app.app_context():
-            db.drop_all()
-            db.create_all()
+        elif request.form['type']:
+            add_item(request.form)
         
     return render_template('index.html')
 
-def add_character():
-    print("Form Submitted. ")
-    return index()
+def add_character (form):
+    char = Character(form['name'], form['hp'], form['weight'])
+    db.session.add(char)
+    db.session.commit()
+
+def add_item (form):
+
+    item = Item(0, form['name'], form['weight'], form['type'])
+
+    if item.type == 'armor':
+        item.base_ac = form['ac']
+        
+    elif item.type == 'weapon':
+        item.hit_bonus = form['hit_bonus']
+        item.dmg_bonus = form['dmg_bonus']
+    
+    #TODO: something of the form "if db contains the item above, don't readd"
+
+    #TODO: else, generate an ID (equal to number of rows in table) and 
+    #       set the item's id to that
+    #       add item with new id to db
 
 if __name__ == '__main__':
     with app.app_context():
+        db.drop_all()
         db.create_all()
+        
     app.run(debug=True)
--- model.py
+++ model.py
@@ -13,30 +13,30 @@ class Character(db.Model):
         self.max_life = life
         self.cur_life = life
         self.carry_weight = weight
-    
-    def __str__ (self):
-        return 'name: ' + self.name + ' life: (' + str(self.cur_life) + '/' + str(self.max_life) + " carry weight: " + str(self.carry_weight)
-
 
 class Item (db.Model):
+    #TODO: add a method to this class that checks if an item that currently exists in the db is identical to one passed in (except ID)
     id: Mapped[int] = mapped_column(primary_key=True, unique=True)
-    name: Mapped[str] = mapped_column(unique=False)
+    name: Mapped[str] = mapped_column(unique=True)
     weight: Mapped[int] = mapped_column()
-    item_type: Mapped[str] = mapped_column(nullable=False)
-    __mapper_args__ = {'polymorphic_on': item_type}
-
-class Weapon (Item):
-    __mapper_args__ = {'polymorphic_identity': 'weapon'}
-    id: Mapped[int] = mapped_column(None, ForeignKey('item.id'), primary_key=True)
+    #TODO: limit type to one of ['item', 'armor', 'weapon']
+    type: Mapped[str] = mapped_column(nullable=False)
     hit_bonus: Mapped[int] = mapped_column()
     dmg_bonus: Mapped[int] = mapped_column()
-
-class Armor (Item):
-    __mapper_args__ = {'polymorphic_identity': 'armor'}
-    id: Mapped[int] = mapped_column(None, ForeignKey('item.id'), primary_key=True)
     base_ac: Mapped[int] = mapped_column()
 
+    def __init__ (self, id, name, weight, type, hit_bonus = None, dmg_bonus = None, base_ac = None):
+        self.id = id
+        self.name = name
+        self.weight = weight
+        self.type = type
+        self.hit_bonus = hit_bonus
+        self.dmg_bonus = dmg_bonus
+        self.base_ac = base_ac
+
 class Inventory(db.Model):
-    name: Mapped[str] = mapped_column(None, ForeignKey('character.name'), primary_key=True)
-    item_id: Mapped[int] = mapped_column(None, ForeignKey('item.id'), unique=True)
+    #transaction id to optimize inventory updates
+    t_id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(None, ForeignKey('character.name'))
+    item_id: Mapped[int] = mapped_column(None, ForeignKey('item.id'))
     quantity: Mapped[int] = mapped_column()
